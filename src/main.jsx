77627e22c0793847406dc82b417d9e6cee8b3b25
PGCgame <79089095+PlayPGC@users.noreply.github.com>
2025-08-08 20:30:18 +0100

    Update main.jsx to load full dashboard

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import ConnectWallet from './ConnectWallet.jsx'
+import MintPiglet from './MintPiglet.jsx'
 import './index.css'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const root = ReactDOM.createRoot(document.getElementById('root'))
+root.render(
   <React.StrictMode>
+    <ConnectWallet />
     <App />
+    <MintPiglet />
   </React.StrictMode>
 )
