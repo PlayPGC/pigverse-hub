7d0ebdb7ef8d6b5a93135826b83cba2edadd23f4
PGCgame <79089095+PlayPGC@users.noreply.github.com>
2025-08-08 21:14:19 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,19 @@
+import React from "react";
+import ConnectWallet from "./ConnectWallet";
+import MintPiglet from "./MintPiglet";
+
 function App() {
   return (
     <div style={{ padding: '2rem', textAlign: 'center' }}>
-      <h1>Welcome to PigVerse Hub</h1>
+      <h1>🐷 Welcome to PigVerse Hub</h1>
       <p>This is your React-powered frontend for NFT minting.</p>
+
+      <div style={{ marginTop: '2rem' }}>
+        <ConnectWallet />
+        <MintPiglet />
+      </div>
     </div>
-  )
+  );
 }
-export default App
+
+export default App;
