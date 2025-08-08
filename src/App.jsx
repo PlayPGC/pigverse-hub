690a7c708583ff3660bde9631027ff832615f892
PGCgame <79089095+PlayPGC@users.noreply.github.com>
2025-08-08 18:32:43 +0100

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div style={{ padding: '2rem', textAlign: 'center' }}>
+      <h1>Welcome to PigVerse Hub</h1>
+      <p>This is your React-powered frontend for NFT minting.</p>
+    </div>
+  )
+}
+export default App
